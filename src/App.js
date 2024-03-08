ea0ab62c39d10257c7c02c93d0125b8609ff5d7e
sachin <sachinkumar854881@gmail.com>
2024-03-09 00:08:36 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const[count, setCount] = useState(0) //destructuring 
+
+    // incre function it increase the value when click button
+    const incre = () =>{
+        setCount(count+1)
+    }
+
+    return(
+        <div>
+            <h1> useState Counter:{count}</h1>
+            <button onClick={incre}>increment</button>
+        </div>
+    )
 }
-
 export default App;
